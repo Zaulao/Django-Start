7da2db5736750bb17ad77047c07f83750b4c66ef
Zaulao <sauloguilhermino@gmail.com>
2020-10-03 16:21:05 -0300

    Models organizing

--- polls/myapp/settings.py
+++ polls/myapp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = '&ko(krt7f#dqznsnhge$x2d=maek$so0x%8k^-)r@1@#nrvnqs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
